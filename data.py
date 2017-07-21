18cb92cfa70b255e5791e3d930371dc51a75a3cd
Hyeokreal <Hyeokryeol.yang@gmail.com>
2017-07-21 11:15:41 +0900

    clean up

--- data.py
+++ data.py
@@ -1,29 +1,32 @@
 import numpy as np
-import tensorflow as tf
 import matplotlib
-
+import os
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from args import get_args
 
 
 class ToyData:
-    def __init__(self, load_saved=False, path_d='./toydata4.npy',
-                 path_c='./toydata_class4.npy', batch_size=32):
-        self.data, self.data_c = self.load_data(load_saved, path_d, path_c)
-        self.data_list = list(self.data)
+    def __init__(self, load_saved=False, path='./data', batch_size=32):
         self.start = 0
         self.end = batch_size
         self.batch_size = batch_size
+        self.path = path
+        self.path_d = '/data_raw.npy'
+        self.path_c = '/data_raw_class.npy'
+        self.args = get_args()
+        self.data, self.data_c = self.load_data(load_saved, path)
+        self.data_list = list(self.data)
 
-    def load_data(self, load_saved, path_d=None, path_c=None):
+
+    def load_data(self, load_saved, path):
         if load_saved:
-            data, data_c = np.load(path_d), np.load(path_c)
+            data, data_c = np.load(path + self.path_d), np.load(path + self.path_c)
             return data, data_c
         else:
-            return self.generate_data()
+            return self.generate_data(number=self.args.num_dots)
 
-    @staticmethod
-    def generate_data(mixtures=4, number=100):
+    def generate_data(self,mixtures=4, number=100):
         means = list()
 
         sq = np.sqrt(mixtures)
@@ -42,17 +45,22 @@ class ToyData:
                                                           [0, 0.001]], number))
         x_1 = np.asarray(classes)
         x = x_1.reshape((-1, 2))
-        # print(x.shape)
-        np.save('./toydata4.npy', x)
-        np.save('./toydata_class4.npy', x_1)
+
+        print(x_1.shape)
+        print(x.shape)
+
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path)
+        np.save(self.path+self.path_d, x)
+        np.save(self.path+self.path_c, x_1)
         return x, x_1
 
-    def draw_x(self, draw_data=None,save=False,figname=None):
+    def draw_x(self, given_data=None, save=False, figname=None):
 
         f, ax = plt.subplots(1)
 
-        if draw_data is not None:
-            data = draw_data
+        if given_data is not None:
+            data = given_data
 
         else:
             data = self.data
@@ -60,13 +68,12 @@ class ToyData:
         cmap = plt.cm.get_cmap('summer', data.shape[0])
         for ix in range(data.shape[0]):
             xy = data[ix].T
-            # print(xy.shape)
             x_pos = xy[0]
             y_pos = xy[1]
             ax.scatter(x_pos, y_pos, c=cmap(ix), marker='.')
 
         if save:
-            plt.savefig(figname)
+            f.savefig(figname)
         else:
             plt.show()
         plt.close()
@@ -74,9 +81,7 @@ class ToyData:
     def get_next_batch(self):
 
         r = self.data[self.start:self.end]
-        # print("start: ", self.start, " end: ", self.end)
 
-        # if self.end == len(self.data)-1:
         if self.end + self.batch_size > len(self.data) - 1:
             self.start = 0
             self.end = self.batch_size
@@ -87,37 +92,10 @@ class ToyData:
 
         return r
 
-    def get_labeled_data(self, label, number):
-
-        start = label * 100
-        end = start + number
-
-        return self.data[start:end]
+    def get_labeled_data(self):
+        return self.data_c
 
 
-if __name__ == '__main__':
-    data = ToyData(load_saved=False, path_d='./toydata4.npy', path_c='./todydata_class4.npy')
-    # label_0 = data.get_labeled_data(0, 32)
-    # label_1 = data.get_labeled_data(1, 32)
-    # label_2 = data.get_labeled_data(2, 32)
-    # label_3 = data.get_labeled_data(3, 32)
-    # label_4 = data.get_labeled_data(4, 32)
-    # label_5 = data.get_labeled_data(5, 32)
-    # label_6 = data.get_labeled_data(6, 32)
-    #
-    # a = []
-    # a.append(label_0)
-    # a.append(label_1)
-    # a.append(label_5)
-    # a.append(label_6)
-    # a = np.asarray(a)
-    # print(a.shape)
-    # a = a.reshape([-1,2])
-    # print(a.shape)
-    data.draw_x()
-
-
-
-    # for i in range(2500):
-    #     print(data.get_next_batch().shape)
-    #
+# if __name__ == '__main__':
+#     data = ToyData(load_saved=False, path='./data')
+#     data.draw_x(given_data=data.data_c, save=True,figname='./figs/fuck.png')
--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layer
 from data import ToyData
+from args import get_args
 
-batch = 30
-z_dim = 2
-input_dim = 2
+arg = get_args()
+
+batch = arg.batch_size
+z_dim = arg.z_dim
+x_dim = arg.x_dim
 
 
 def lrelu(x, leak=0.2, name="lrelu"):
@@ -14,32 +17,41 @@ def lrelu(x, leak=0.2, name="lrelu"):
         return f1 * x + f2 * abs(x)
 
 
+def dense(input, out_num, activation_fn=None, scope='dense', batch_norm=False, is_tr=None):
+    scope1 = scope
+    scope2 = scope + '_bn'
+    scope3 = scope + '_relu'
+    fc = layer.fully_connected(input, out_num, activation_fn=None, scope=scope1)
+    if batch_norm:
+        bn = layer.batch_norm(fc, activation_fn=activation_fn, is_training=is_tr, scope=scope2)
+
+        return bn
+
+    if activation_fn is not None:
+        out = activation_fn(fc, name=scope3)
+        return out
+
+    return fc
+
+
 # input: real image x -> return: latent space z
 # for toy example : input: (N, 2) -> return: latent z (N,2)
 # encoder
 # fully -> dependent layer
-'''
-def Q(x):
-    with tf.variable_scope('GQ'):
-        fc1 = layer.fully_connected(x, 400, activation_fn=tf.nn.relu, scope='fc1')
-        fc2 = layer.fully_connected(fc1, 400, activation_fn=tf.nn.relu, scope='fc2')
-        mu = layer.fully_connected(fc2, z_dim, activation_fn=None, scope='mu')
-        log_sig_sq = layer.fully_connected(fc2, z_dim, activation_fn=None, scope='log_sig_sq')
-        e = tf.random_normal([batch, z_dim])
-        out = tf.add(mu, tf.multiply(tf.exp(log_sig_sq / 2), e))
-    return out
-'''
-
-
 # split layer
-def Q(x):
+# encoder
+def Q(x, is_tr=None):
     with tf.variable_scope('GQ'):
-        fc1 = layer.fully_connected(x, 400, activation_fn=tf.nn.relu, scope='fc1')
-        fc2 = layer.fully_connected(fc1, 400, activation_fn=tf.nn.relu, scope='fc2')
-        fc3 = layer.fully_connected(fc2, 2 * z_dim, activation_fn=tf.nn.relu, scope='fc3')
-        mu, log_sig_sq = tf.split(fc3, 2, axis=1)
-        # mu = layer.fully_connected(fc2, z_dim, activation_fn=None, scope='mu')
-        # log_sig_sq = layer.fully_connected(fc2, z_dim, activation_fn=None, scope='log_sig_sq')
+        fc1 = dense(x, arg.E_layer, activation_fn=tf.nn.relu, scope='fc1', batch_norm=True,
+                    is_tr=is_tr)
+        fc2 = dense(fc1, arg.E_layer, activation_fn=tf.nn.relu, scope='fc2', batch_norm=True,
+                    is_tr=is_tr)
+        if arg.param_trick == 'split':
+            fc3 = dense(fc2, 2 * z_dim, activation_fn=tf.nn.relu, scope='fc3')
+            mu, log_sig_sq = tf.split(fc3, 2, axis=1)
+        else:
+            mu = dense(fc2, z_dim, scope='mu')
+            log_sig_sq = dense(fc2, z_dim, scope='log_sig_sq')
         e = tf.random_normal([batch, z_dim])
         out = tf.add(mu, tf.multiply(tf.exp(log_sig_sq / 2), e))
     return out
@@ -48,23 +60,29 @@ def Q(x):
 # input: latent z -> return: fake image x
 # for toy example : input: z-(N, 2) -> return: fake x (N,2)
 # decoder
-def P(z):
+def P(z, is_tr):
     with tf.variable_scope('GP'):
-        fc1 = layer.fully_connected(z, 400, activation_fn=tf.nn.relu, scope='fc1')
-        fc2 = layer.fully_connected(fc1, 400, activation_fn=tf.nn.relu, scope='fc2')
-        fc3 = layer.fully_connected(fc2, 400, activation_fn=tf.nn.relu, scope='fc3')
-        out = layer.fully_connected(fc3, input_dim, activation_fn=None, scope='out')
+        fc1 = dense(z, arg.G_layer, activation_fn=tf.nn.relu, scope='fc1', batch_norm=True,
+                    is_tr=is_tr)
+        fc2 = dense(fc1, arg.G_layer, activation_fn=tf.nn.relu, scope='fc2', batch_norm=True,
+                    is_tr=is_tr)
+        fc3 = dense(fc2, arg.G_layer, activation_fn=tf.nn.relu, scope='fc3', batch_norm=True,
+                    is_tr=is_tr)
+        out = dense(fc3, x_dim, activation_fn=None, scope='out')
     return out
 
 
 # input -> x, z ==> concat
-def D(x, z):
+def D(x, z, is_tr):
     with tf.variable_scope('D'):
         input = tf.concat([x, z], axis=1)
-        fc1 = layer.fully_connected(input, 200, activation_fn=tf.nn.relu, scope='fc1')
-        fc2 = layer.fully_connected(fc1, 200, activation_fn=tf.nn.relu, scope='fc2')
-        fc3 = layer.fully_connected(fc2, 200, activation_fn=tf.nn.relu, scope='fc3')
-        out = layer.fully_connected(fc3, 1, activation_fn=tf.nn.tanh, scope='out')
+        fc1 = dense(input, arg.D_layer, activation_fn=tf.nn.relu, scope='fc1', batch_norm=True,
+                    is_tr=is_tr)
+        fc2 = dense(fc1, arg.D_layer, activation_fn=tf.nn.relu, scope='fc2', batch_norm=True,
+                    is_tr=is_tr)
+        fc3 = dense(fc2, arg.D_layer, activation_fn=tf.nn.relu, scope='fc3', batch_norm=True,
+                    is_tr=is_tr)
+        out = dense(fc3, 1, activation_fn=tf.nn.sigmoid, scope='out')
     return out
 
 
